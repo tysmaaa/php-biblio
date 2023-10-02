c0b6df04311eb512f55b3dcc5d96ec68dbf09c47
huot-marchandm <huot-marchand.matys@lyceepergaud.fr>
2023-10-02 10:14:49 +0200

    initialisation projet

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'vendor_name/php-bibliotheque',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '4f7443d57fcc4e758f62389ffe19308c6f1fa3ef',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'vendor_name/php-bibliotheque' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '4f7443d57fcc4e758f62389ffe19308c6f1fa3ef',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
