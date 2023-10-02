03de57f2c4e24c1116c98890c2180c052b735ffa
huot-marchandm <huot-marchand.matys@lyceepergaud.fr>
2023-10-02 11:12:37 +0200

    realisation diagramme de classe

---
+++ tests/unitaire/emprunt-test.php
@@ -0,0 +1,8 @@
+<?php
+require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . "/utils/couleurs.php";
+echo PHP_EOL;
+echo GREEN_BACKGROUND.BLACK;
+echo "Tests : classe Adherent";
+echo RESET;
+echo PHP_EOL;
